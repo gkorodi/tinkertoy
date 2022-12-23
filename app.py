f14566777b690d105ac9541b30575d94e2b476ec
Gabor Korodi <gak486@g.harvard.edu>
2022-12-22 19:21:04 -0500

    Getting ready

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 @app.get("/celsius/{degree}")
-def convert_celsius(degree: int, q: Union[str, None] = None):
+def convert_celsius(degree: int):
     degree_in_f = (degree * (9/5)) + 32
-    return {"degree_in_celsius": degree, "degree_in_fahrenheit": degree_in_f, "q": q}
+    return {"degree_in_celsius": degree, "degree_in_fahrenheit": degree_in_f}
 
 @app.get("/fahrenheit/{degree}")
-def convert_fahrenheit(degree: int, q: Union[str, None] = None):
+def convert_fahrenheit(degree: int):
     degree_in_c = (degree - 32) * (5/9)
-    return {"degree_in_fahrenheit": degree, "degree_in_c": degree_in_c, "q": q}
+    return {"degree_in_fahrenheit": degree, "degree_in_c": degree_in_c}
 
--- tinkertoy.sh
+++ tinkertoy.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env sh
 
-up() {
-  SCRIPT_NAME=$(basename $0 .sh)
-  SCRIPT_DIR=$(dirname $0)
+SCRIPT_NAME=$(basename $0 .sh)
+SCRIPT_DIR=$(dirname $0)
 
+run() {
   cd $SCRIPT_DIR
-  uvicorn app:app --reload --reload --host 0.0.0.0 --port 82
+  uvicorn app:app --reload --port 9001 #--host 0.0.0.0 --port 82
 }
+
+build() {
+  cd $SCRIPT_DIR
+  docker build . --tag tinkertoy:latest --tag tinkertoy:$(date +%s)
+}
+
+up() {
+  cd $SCRIPT_DIR
+  docker run -it --rm --name tinkertoy-app --publish "82:80" tinkertoy:latest
+}
+
+dockerpush() {
+  docker login --username gaborkorodi #password is in ~/.docker/.hub_token
+  docker tag tinkertoy:latest gaborkorodi/tinkertoy:latest
+  docker push gaborkorodi/tinkertoy:latest
+}
+
+
 
 $*
